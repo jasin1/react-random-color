48470ff547e27396fea385a5dabfb38595af702d
jasin1 <jasin.tairaidrissi@gmail.com>
2025-05-07 07:10:18 +0200

    random color generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [myColor, setMyColor] = useState(0)
+  
+  function randomHex(){
+    const myHex = '0123456789ABCDEF';
+    let color = '#';
+    for(let i=0; i < 6; i++){
+     color += myHex[Math.floor(Math.random()*16)];
+
+    }
+    document.body.style.backgroundColor = color;
+    setMyColor(color);
+    console.log(color);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={randomHex}>
+          Random Color
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{myColor}</h1>
     </>
   )
 }
